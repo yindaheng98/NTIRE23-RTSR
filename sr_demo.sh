1bdf7cf8b8379211b2e9f6ef951a43c51568ab3c
yindaheng98 <yindaheng98@gmail.com>
2023-03-19 16:11:55 +0800

    save models

--- sr_demo.sh
+++ sr_demo.sh
@@ -5,5 +5,5 @@ conda activate NTIRE23
 ROOT=$(dirname $0)
 mkdir -p ./results
 
-CUDA_VISIBLE_DEVICES=0 python demo/sr_demo.py --submission-id SEU_CNII --checkpoint /home/data/NTIRE2022_ESR/save_models/RFDN_-11_trainLSDIRX2_Param0.11M/model/model_latest.pt.pth --scale 2 --lr-dir /home/data/dataset/DIV2K/DIV2K_valid_HR --save-sr
-CUDA_VISIBLE_DEVICES=0 python demo/sr_demo.py --submission-id SEU_CNII --checkpoint /home/data/NTIRE2022_ESR/save_models/RFDN_-11_trainLSDIRX3_Param2.20M/model/model_latest.pt.pth --scale 3 --lr-dir /home/data/dataset/DIV2K/DIV2K_valid_HR --save-sr
+CUDA_VISIBLE_DEVICES=0 python demo/sr_demo.py --submission-id SEU_CNII --checkpoint /home/data/NTIRE2022_ESR/save_models/RFDN_-10_trainLSDIRX2_Param0.27M/model/model_latest.pt.pth --scale 2 --lr-dir /home/data/dataset/DIV2K/DIV2K_valid_HR --save-sr
+CUDA_VISIBLE_DEVICES=0 python demo/sr_demo.py --submission-id SEU_CNII --checkpoint /home/data/NTIRE2022_ESR/save_models/RFDN_-10_trainLSDIRX3_Param0.27M/model/model_latest.pt.pth --scale 3 --lr-dir /home/data/dataset/DIV2K/DIV2K_valid_HR --save-sr
