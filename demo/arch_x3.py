a8126ec4b1b8542cdb58495f23e4001c20d92bd7
yindaheng98 <yindaheng98@gmail.com>
2023-03-20 19:31:53 +0800

    explain them

--- demo/arch_x3.py
+++ demo/arch_x3.py
@@ -1,5 +1,13 @@
 import torch
 
+"""
+!!!Important!!!
+The structure of our model is in demo/utils/models.
+Since applied auto-pruning our model, models in .pth file have same structure of the models in demo/utils/models,
+but have different shape of weights.
+So our model can only be loaded by `torch.load` rather than `model.load_state_dict`
+"""
+
 
 def srmodel():
     return torch.load("PRFDN_x3.pth")
