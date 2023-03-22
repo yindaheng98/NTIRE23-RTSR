103a7f6c983d1844d927de1bd34772297973401a
yindaheng98 <yindaheng98@gmail.com>
2023-03-22 15:19:06 +0800

    just for submission

---
+++ demo/arch.py
@@ -0,0 +1,13 @@
+import torch
+
+"""
+To organizers:
+The structure of our model is in demo/utils/models.
+Since applied auto-pruning our model, models in .pth file have same structure of the models in demo/utils/models,
+but have different shape of weights.
+So our model can only be loaded by `torch.load` rather than `model.load_state_dict`
+"""
+
+
+def srmodel():
+    return torch.load("checkpoint.pth")
