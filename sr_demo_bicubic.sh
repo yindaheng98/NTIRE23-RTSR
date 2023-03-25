fde84f1fbb15e244089799db8d92ea7cdd252074
yindaheng98 <yindaheng98@gmail.com>
2023-03-25 16:22:43 +0800

    生成插值用于测试

---
+++ sr_demo_bicubic.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+export LD_LIBRARY_PATH=/home/seu/miniconda3/envs/NTIRE23/lib/python3.9/site-packages/torch/lib/../../nvidia/cublas/lib/:$LD_LIBRARY_PATH
+eval "$(conda shell.bash hook)"
+conda activate NTIRE23
+ROOT=$(dirname $0)
+mkdir -p ./results
+
+CUDA_VISIBLE_DEVICES=0 python demo/sr_demo.py --submission-id SEU_CNIIx2bicubic \
+  --checkpoint demo/PRFDN_x2.pth \
+  --scale 2 \
+  --lr-dir /home/data/dataset/NTIRE23-RTSR/LR2 \
+  --save-sr --bicubic
+CUDA_VISIBLE_DEVICES=0 python demo/sr_demo.py --submission-id SEU_CNIIx3bicubic \
+  --checkpoint demo/PRFDN_x3.pth \
+  --scale 3 \
+  --lr-dir /home/data/dataset/NTIRE23-RTSR/LR3 \
+  --save-sr --bicubic
